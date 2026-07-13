8aed3972741acf2b6e23964189414486a49d5808
bekhzodrasulov <bekhzodr2001@gmail.com>
2026-07-13 22:58:03 +0500

    animation

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function tick() {
 tick();
 setInterval(tick, 1000);
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+}, {
+    threshold: 0.2
+});
+
+document.querySelectorAll(".animate").forEach((el) => {
+    observer.observe(el);
+});
